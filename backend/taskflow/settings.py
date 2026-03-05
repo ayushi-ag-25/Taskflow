b31dba404667d1f6b46c06a42af4100e8430d120
ayushi-ag-25 <ayushiagrawal2507@gmail.com>
2026-03-05 10:19:24 +0530

    add railway.json config

--- backend/taskflow/settings.py
+++ backend/taskflow/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-162b&u%)+fxfc_*j(k!&1j$b^#zl6)u-%$1(mslcthxf+32tp%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
